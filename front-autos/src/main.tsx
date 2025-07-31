8f24179c907802b7ab85b49799c132d15b22ba3c
Stefano Arias <stefano9405@hotmail.com>
2025-07-31 12:07:20 -0500

    FEAT/Estructura inicial del front.

---
+++ front-autos/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { store } from './store/store';
+import App from './App';
+import './styles/index.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
+const queryClient = new QueryClient();
+const theme = createTheme();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
