582be1de0363112f7161715c2459b24570a30cb7
Stefano Arias <stefano9405@hotmail.com>
2025-07-31 12:42:45 -0500

    FIX/Refactorización inicio de front, tailwind con vite.

--- front-autos/src/main.tsx
+++ front-autos/src/main.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { store } from './store/store';
-import App from './App';
-import './styles/index.css';
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const queryClient = new QueryClient();
-const theme = createTheme();
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
